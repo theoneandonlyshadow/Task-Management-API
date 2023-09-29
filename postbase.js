46d202ba307c7be3b303e92ba25aa5fd4d271958
Madhav Nair <119957011+theoneandonlyshadow@users.noreply.github.com>
2023-09-29 16:10:48 +0530

    Update postbase.js

--- postbase.js
+++ postbase.js
@@ -2,11 +2,11 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
 
-    user: "postgres",
-    password: "root",
-    database: "techcorp",
-    host: "localhost",
-    port: 5432
+    user: "postgres", //username
+    password: "root", //password
+    database: "techcorp", //database name
+    host: "localhost", //host (localhost by default)
+    port: 5432 //port for psql
 });
 
 module.exports = pool;
