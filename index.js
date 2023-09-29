0e26ae372c8bd8c6e89b48b25a513d37b5249d34
Madhav Nair <119957011+theoneandonlyshadow@users.noreply.github.com>
2023-09-29 17:48:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const pool = require("./postbase.js");
 const port = 8070;
+const fetch = require('node-fetch');
 const router = express.Router();
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
